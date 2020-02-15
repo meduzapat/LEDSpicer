d1e0fe407f54807b8caef6cedbe0f21b51a40fd3
Patricio Rossi <meduzapat@netscape.net>
2020-02-15 17:53:52 -0500

    Improvement
    * Added default element names on the pin layout

--- src/devices/WolfWareTech/Howler.cpp
+++ src/devices/WolfWareTech/Howler.cpp
@@ -37,22 +37,26 @@ void Howler::drawHardwarePinMap() {
 	cout
 		<< getFullName() << " Pins " << HOWLER_LEDS << endl
 		<< "Hardware pin map:" << endl
+		<< "               J 1          J 2           " << endl
 		<< "G + X X X X  " << (int)*getLed(2) << "  " << (int)*getLed(1) << "  " << (int)*getLed(0) << " + +  "
 		<< (int)*getLed(3) << "  " << (int)*getLed(4) << "  " << (int)*getLed(5) << " X X X X + G" << endl;
-	for (uint8_t c = 0, t = (LEDs.size() / 2) - 9; c < t ; c+=3) {
+	for (uint8_t c = 0, c2 = 1, t = (LEDs.size() / 2) - 9; c < t ; c+=3, ++c2) {
 		cout
 			<< "+" << f << " +" << endl
 			<< (int)*getLed(c + 12) << f << (int)*getLed(t + c + 12) << endl
-			<< (int)*getLed(c + 13) << f << (int)*getLed(t + c + 13) << endl
+			<< (int)*getLed(c + 13) << " B" << static_cast<int>(c2)
+			<< (c2 < 10 ? " " : "")
+			<< "                                  B" << static_cast<int>(c2 + 13) << " "
+			<< (int)*getLed(t + c + 13) << endl
 			<< (int)*getLed(c + 14) << f << (int)*getLed(t + c + 14) << endl
 			<< "X" << f << " X" << endl;
 	}
 	cout
 		<< "+" << f << " +" << endl
 		<< (int)*getLed(90) << f << (int)*getLed(93) << endl
-		<< (int)*getLed(91) << f << (int)*getLed(94) << endl
+		<< (int)*getLed(91) << " HP 1                                HP 2 " << (int)*getLed(94) << endl
 		<< (int)*getLed(92) << f << (int)*getLed(95) << endl
-		<< "G" << f << " G" << endl
+		<< "G              J 3         J 4              G" << endl
 		<< "G + X X X X  " << (int)*getLed(8) << "  " << (int)*getLed(7) << "  " << (int)*getLed(6) << " + + "
 		<< (int)*getLed(9) << " " << (int)*getLed(10) << " " << (int)*getLed(11) << " X X X X + G" << endl << endl;
 }
