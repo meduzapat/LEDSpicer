229915eac40064ff8aaba3e752563eb1282c12e5
bzzrd <bzzrd@protonmail.com>
2021-02-08 18:32:03 +0100

    Fixed error handling for adalight and configure help message

--- src/devices/Adalight/Adalight.cpp
+++ src/devices/Adalight/Adalight.cpp
@@ -90,23 +90,22 @@ void Adalight::transfer() const {
 /* Helper function for error handling. */
 int Adalight::check(enum sp_return result)
 {
-        /* For this example we'll just exit on any error by calling abort(). */
         char *error_message;
 
         switch (result) {
         case SP_ERR_ARG:
-                printf("Error: Invalid argument.\n");
+                throw Error("Adalight Error: Invalid argument.\n");
                 abort();
         case SP_ERR_FAIL:
                 error_message = sp_last_error_message();
-                printf("Error: Failed: %s\n", error_message);
+                throw Error("Adalight Error: Failed: %s\n", error_message);
                 sp_free_error_message(error_message);
                 abort();
         case SP_ERR_SUPP:
-                printf("Error: Not supported.\n");
+                throw Error("Adalight Error: Not supported.\n");
                 abort();
         case SP_ERR_MEM:
-                printf("Error: Couldn't allocate memory.\n");
+                throw Error("Adalight Error: Couldn't allocate memory.\n");
                 abort();
         case SP_OK:
         default:
