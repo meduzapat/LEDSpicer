56ae70a63af1dcc7ddc0e9caeb6d1da9500ad4b3
Patricio Rossi <meduzapat@netscape.net>
2024-02-08 17:56:26 -0500

    27: Update Dates
    Task-Url: https://github.com/meduzapat/LEDSpicer/issues/27
    
    Replaced old dates

--- src/devices/Ultimarc/NanoLed.cpp
+++ src/devices/Ultimarc/NanoLed.cpp
@@ -3,7 +3,7 @@
  * @file      NanoLed.cpp
  * @since     Mar 11, 2019
  * @author    Patricio A. Rossi (MeduZa)
- * @copyright Copyright © 2018 - 2020 Patricio A. Rossi (MeduZa)
+ * @copyright Copyright © 2018 - 2024 Patricio A. Rossi (MeduZa)
  */
 
 #include "NanoLed.hpp"
